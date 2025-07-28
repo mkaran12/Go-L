ac9c7ae09919c5362dc1f8e612c65ea4add25f29
Moses Karani <mk@Mosess-MacBook-Pro.local>
2025-07-28 19:20:02 +0300

    learning go concept

---
+++ basics/data_type.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// DataType represents a custom data type with a name and a value.
+func main() {
+	fmt.Println("DataType package initialized")
+	fmt.Println("1000 X 200= ", 1000*200)
+	fmt.Println("100/3-4*23=", 100/3-4*23)
+
+	fmt.Println(false && true)
+	fmt.Println(false || true)
+	fmt.Println(!false)
+
+}
